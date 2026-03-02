4ce7fda15e80a029c9dc27d335bd5ad535d9cb4b
Aqib Mehmood <aqibmehmood767@gmail.com>
2026-03-02 14:23:11 +0500

    added pattern22

--- src/Printing_Patterns/Pattern22.java
+++ src/Printing_Patterns/Pattern22.java
@@ -2,54 +2,53 @@ package Printing_Patterns;
 
 /*Print the below pattern
 
- ****
- *  *
- *  *
- ****
+5 5 5 5 5 5 5 5 5
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
 
  */
+
+import java.util.Arrays;
 
 public class Pattern22 {
 
     static void main() {
-        pattern22(2);
+        pattern22(5);
     }
 
 
     static void pattern22(int n) {
-    int spaces = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int k = i; k <= n; k++) {
-            System.out.print("*");
-            if (k == n && spaces != 0) {
-                for (int j = 0; j < spaces; j++) {
-                    System.out.print(" ");
-                }
-            }
-        }
-        for (int k = i; k <= n; k++) {
-            System.out.print("*");
-        }
-        spaces += 2;
-        System.out.println();
-    }
+        int holdValue = n;
+        int[] arr = new int[(n * 2) - 1];
+        Arrays.fill(arr, n);
 
-    //For Reverse
-    int reverseSpace = (n-1)*2;
-    for (int i = 1; i <= n; i++) {
-        for (int k = 1; k <= i; k++) {
-            System.out.print("*");
-            if (k == i && reverseSpace != 0) {
-                for (int j = 0; j < reverseSpace; j++) {
-                    System.out.print(" ");
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < arr.length - i; j++) {
+                arr[j] = holdValue;
             }
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+            holdValue--;
         }
-        for (int k = 1; k <= i; k++) {
-            System.out.print("*");
+
+        holdValue = holdValue+2;
+        for (int i = n-1; i > 0; i--) {
+            for (int j = i-1; j < arr.length-i; j++) {
+                arr[j] = holdValue;
+            }
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+            holdValue++;
         }
-        reverseSpace -= 2;
-        System.out.println();
     }
-}
 }

b7e165a51ad06705de22142d93bb02ca82e3f000
aqibmehmood <aqibmehmood11969@gmail.com>
2026-07-22 00:36:06 +0500

    half done pattern19

--- src/Printing_Patterns/Pattern19.java
+++ src/Printing_Patterns/Pattern19.java
@@ -2,23 +2,24 @@ package Printing_Patterns;
 
 /*Print the below pattern
 
-***********
-****  ****
-***    ***
-**      **
-*        *
-*        *
-**      **
-***    ***
-****  ****
-**********
+ ***********
+ ****  ****
+ ***    ***
+ **      **
+ *        *
+ *        *
+ **      **
+ ***    ***
+ ****  ****
+ **********
 
-*/
+ */
 
 
 public class Pattern19 {
     static void main() {
-        pattern19(5);
+//        pattern19(5);
+        pattern(5);
     }
 
     static void pattern19(int n) {
@@ -40,7 +41,7 @@ public class Pattern19 {
         }
 
         //For Reverse
-        int reverseSpace = (n-1)*2;
+        int reverseSpace = (n - 1) * 2;
         for (int i = 1; i <= n; i++) {
             for (int k = 1; k <= i; k++) {
                 System.out.print("*");
@@ -55,6 +56,38 @@ public class Pattern19 {
             }
             reverseSpace -= 2;
             System.out.println();
+        }
+    }
+
+
+    //    Striver
+    public static void pattern(int n) {
+        int spaces = 0;
+        int loop = n;
+        for (int i = 0; i <= n * 2; i++) {
+
+            //stars
+            for (int l = 1; l <= loop-i; l++) {
+                System.out.print("*");
+            }
+
+            //spaces
+            for (int k = 1; k <= spaces; k++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int l = 1; l <= loop-i; l++) {
+                System.out.print("*");
+            }
+            if (i < n)
+                spaces = spaces + 2;
+            else{
+                spaces = spaces - 2;
+                loop = loop+2;
+            }
+            System.out.println();
+
         }
     }
 }
